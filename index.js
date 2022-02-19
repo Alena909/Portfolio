b54e572c4151985dbe064630a7b8eecf482828e3
Yelena Guthrie <yelena.guthrie@gmail.com>
2022-02-18 20:16:32 -0600

    added light mode and new site

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const cardContainers = document.querySelectorAll(".card-container");
+const modeSwitch = document.querySelector('input[type="checkbox"]');
+const iconSwitch = document.getElementById("icon-switch");
 
 cardContainers.forEach((card) => {
   card.addEventListener("click", (e) => {
@@ -9,3 +11,21 @@ cardContainers.forEach((card) => {
     cardContainer.classList.add("active");
   });
 });
+function mode(isLight) {
+  iconSwitch.children[0].textContent = isLight ? "Light Mode" : "Dark Mode";
+  isLight
+    ? iconSwitch.children[1].classList.replace("fa-moon", "fa-sun")
+    : iconSwitch.children[1].classList.replace("fa-sun", "fa-moon");
+}
+
+function modeChange(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "light");
+    mode(true);
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+    mode(false);
+  }
+}
+
+modeSwitch.addEventListener("change", modeChange);
